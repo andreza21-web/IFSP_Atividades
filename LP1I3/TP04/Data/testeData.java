e205ca2dccf458939c8e0d5d7e7a91c8e1c93276
Andreza do Nascimento Pessoa <79468757+andreza21-web@users.noreply.github.com>
2021-07-02 18:58:46 -0300

    Add files via upload

---
+++ LP1I3/TP04/Data/testeData.java
@@ -0,0 +1,14 @@
+
+package Data;
+
+public class testeData {
+    public static void main(String[] args) {
+		
+     var data = new Data();
+	System.out.println(data.mostra1());
+	System.out.println(data.mostra2());
+	System.out.println("Dias transcorridos: " + data.diasTranscorridos());
+	data.apresentaDataAtual();
+	}
+
+}
